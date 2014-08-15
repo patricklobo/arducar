a35ce63fd496810e89f8a35fc99ff8eae65be98b
Patrick Lobo <Patrick Lobo@Lobo-Server>
2014-08-15 12:52:02 -0300

    Grilo: Alteria ajkskjjk

--- class/UsuarioBD.php
+++ class/UsuarioBD.php
@@ -12,5 +12,25 @@
  * @author patricklobo
  */
 class UsuarioBD {
-    //put your code here
+    private $id;
+    private $nome;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+
+    
 }
